ada56913eb2477675b232fbbd90ad845d50bed00
Juma <juma_ad@hotmail.com>
2024-02-28 08:45:29 +0400

    Update 704. Binary Search.cpp

--- Neetcode Roadmap/Binary Search/704. Binary Search.cpp
+++ Neetcode Roadmap/Binary Search/704. Binary Search.cpp
@@ -1,19 +1,16 @@
-wrong currently...
-
 class Solution {
 public:
     int search(vector<int>& n, int target) {
         std::sort(n.begin(), n.end());
 
+
+
         int indexLeft = 0;
         int indexRight = n.size()-1;
-       
         
         while(indexLeft <= indexRight)
         {
-            cout << indexLeft << indexRight << ci << endl;
-            
-            ci = (indexLeft + (indexRight-indexLeft))/2;
+            int ci = indexLeft + (indexRight - indexLeft)/2;//(indexLeft + (indexRight - indexLeft))/2; my issue was the bigger outside brackets!!
 
             if (target == n[ci])
             {
